2bd2ca16ed63e00ebbe04eb132a7c99235ce43b6
AoyamaNaoki <45162737+AoyamaNaoki@users.noreply.github.com>
2018-12-10 12:08:19 +0900

    5枚のカードで最も強いカードの出力

--- src/Run.java
+++ src/Run.java
@@ -1,10 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Run {
 
 	public static void main(String[] args) {
 		Deck deck = new Deck();
 		deck.InitDeck();
 		int maxNumber = 0;
-
+		int count = 3;
 		// 残り枚数があるときはカードを引く
 		while (deck.Nokori > 0) {
 			Card card = deck.CardDraw();
@@ -33,8 +36,10 @@ public class Run {
 		// カードの大小比較
 		Deck deck2 = new Deck();
 		deck2.InitDeck();
-		Card card1 = deck.CardDraw();
-		Card card2 = deck.CardDraw();
+		Card card1 = deck2.CardDraw();
+		Card card2 = deck2.CardDraw();
+
+		System.out.println("【カード2枚の強さの比較】");
 
 		if (card1.CompareCard(card2) == 1) {
 			System.out.println("カード1はカード2に対し勝ちです");
@@ -45,60 +50,40 @@ public class Run {
 		} else {
 			System.out.println("そのパターンは存在しません");
 		}
+		System.out.println();
 
-//		if (card1.Compare(card2) == -1) {
-//			System.out.println("カード1の数値はカード2より" + (card2.getNumber() - card1.getNumber()) + "小さいです");
-//		} else if (card1.Compare(card2) == 0) {
-//			System.out.println("カード1とカード2の数値は等しいです");
-//			if (card1.CompareMark(card2) == 1) {
-//				System.out.println("カード1のマークがカード2より強いので勝ちです");
-//			} else if (card1.CompareMark(card2) == -1) {
-//				System.out.println("カード1のマークがカード2より弱いので負けです");
-//			} else {
-//				System.out.println("そのパターンは存在しません");
-//			}
-//		} else {
-//			System.out.println("カード1の数値はカード2より" + (card1.getNumber() - card2.getNumber()) + "大きいです");
-//		}
+		Deck deck3 = new Deck();
+		deck3.InitDeck();
+		Card card3 = deck3.CardDraw();
+		Card card4 = deck3.CardDraw();
+		Card card5 = deck3.CardDraw();
+		Card card6 = deck3.CardDraw();
+		Card card7 = deck3.CardDraw();
+		List<Card> cards3 = new ArrayList<Card>();
+		cards3.add(card3);
+		cards3.add(card4);
+		cards3.add(card5);
+		cards3.add(card6);
+		cards3.add(card7);
+		Card strongCard;
 
-//		// カードを5枚引いて最大のカードを出す
-//		Deck deck3 = new Deck();
-//		deck3.InitDeck();
-//		Card card3 = deck.CardDraw();
-//		Card card4 = deck.CardDraw();
-//		Card card5 = deck.CardDraw();
-//		Card card6 = deck.CardDraw();
-//		Card card7 = deck.CardDraw();
-//		List<Card> cards3 = new ArrayList<Card>();
-//		cards3.add(card3);
-//		cards3.add(card4);
-//		cards3.add(card5);
-//		cards3.add(card6);
-//		cards3.add(card7);
-//		Card strongCard = cards3.get(0);
-//		List<Card> strongCards = new ArrayList<Card>();
-//		int count = 3;
-//		while (count < strongCards.size()) {
-//			if (card3.Compare(card4) == 1)
-//				;
-//
-//			count += 1;
-//		}
-
-//		System.out.println("強いカードは" + strongCard);
-
-//		if (strongCards.size() == 1) {
-//			System.out.println("一番強いカードは" + strongCard.getNumber());
-//		} else if (strongCards.size() > 1) {
-//			System.out.println("数字被りあり");
-//		}
-
-//		System.out.println(card3.getNumber());
-//		System.out.println(card4.getNumber());
-//		System.out.println(card5.getNumber());
-//		System.out.println(card6.getNumber());
-//		System.out.println(card7.getNumber());
-
+		if (card3.CompareCard(card4) == 1) {
+			strongCard = card3;
+		} else {
+			strongCard = card4;
+		}
+		if (strongCard.CompareCard(card5) == -1) {
+			strongCard = card5;
+		}
+		if (strongCard.CompareCard(card6) == -1) {
+			strongCard = card6;
+		}
+		if (strongCard.CompareCard(card7) == -1) {
+			strongCard = card7;
+		}
+		System.out.println("【カードを5枚引いて最も強いカードの出力】");
+		System.out.println("5枚のカードの中で最も強いのは " + strongCard.getMark() + " の " + strongCard.getNumber() + " です ");
+		System.out.println();
 	}
 
 }
